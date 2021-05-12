5c9bcef1b7bc04d1494da8c82589bc51915e1c39
Surekha Dhote <65859795+Surekhadhote@users.noreply.github.com>
2021-05-12 14:05:50 +0530

    Create pascal's triangle.cpp

---
+++ pascal's triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int n;
+cin>>n;
+    int c=1;
+    for(int i=1;i<=n;i++){
+        c=1;
+        for(int j=1;j<=i;j++){
+            cout<<c<<" ";
+            c = c * (i - j) / j;
+        }cout<<endl;
+    }
+
+    return 0;
+}
