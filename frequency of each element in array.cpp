c14ce37ca26b5f8c671847914089f085ab92e323
Surekha Dhote <65859795+Surekhadhote@users.noreply.github.com>
2021-05-13 13:18:27 +0530

    Create frequency of each element in array.cpp

---
+++ frequency of each element in array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void countfreq(int a[],int n){
+    unordered_map<int,int> mp;
+    for(int i=0;i<n;i++){
+        mp[a[i]]++;
+    }
+    for(auto i:mp){
+        cout<<i.first<"frequency is "<<i.second;
+    }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+      cin>>a[i];
+  }
+  countfreq(a,n);
+     return 0;
+}
