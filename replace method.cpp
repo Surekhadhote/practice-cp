7e6de88322c3f847b25271d4ad166234c6be1d6a
Surekha Dhote <65859795+Surekhadhote@users.noreply.github.com>
2021-05-20 15:58:57 +0530

    Create replace method.cpp

---
+++ replace method.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+         std::replace(s.begin(), s.end(),' ', '\0');
+     
+        return s;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
