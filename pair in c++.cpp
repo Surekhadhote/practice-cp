010c8a4bc2840d7dea01ea992047079763dccbc3
Surekha Dhote <65859795+Surekhadhote@users.noreply.github.com>
2021-05-23 17:51:33 +0530

    Create pair in c++.cpp

---
+++ pair in c++.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    
+    sort(a,a+n);
+    pair<ll,ll> s ;
+    s.first = a[0];
+    s.second = a[n-1];
+    return s;
+}
