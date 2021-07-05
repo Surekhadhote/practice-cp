c4d7dc3b87644a9fe6629a954546d8e02c4a9a17
Surekha Dhote <65859795+Surekhadhote@users.noreply.github.com>
2021-07-05 11:45:13 +0530

    Create valid palindrome string.cpp

---
+++ valid palindrome string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+      int i=0,j=s.length()-1;
+        while(i<j){
+if(!(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9')||!(s[j]>='a'&&s[j]<='z'||s[j]>='0'&&s[j]<='9'))
+ { 
+   if( !(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9'))
+           i++;
+                
+   if(!(s[j]>='a'&&s[j]<='z'||s[j]>='0'&&s[j]<='9'))
+            j--;
+                
+        }
+            
+  else{           
+      if(s[i]!=s[j])
+         return false;
+            
+        i++;
+        j--;
+      }     
+           
+           
+        }
+        return true;
+    }
+};
